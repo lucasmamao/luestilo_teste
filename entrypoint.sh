3e8f5f148811b0b467bee68a0cba6cc7cc38ab8c
lucasmamao <42155522+lucasmamao@users.noreply.github.com>
2025-05-26 20:24:03 -0300

    Adicionando vercel.json para deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for PostgreSQL to be ready..."
-while ! nc -z luestilo_database 5432; do
-  sleep 0.5
-done
-echo "PostgreSQL is ready!"
-
 echo "Running database migrations..."
-poetry run alembic upgrade head
+poetry run alembic upgrade head 
 echo "Migrations complete!"
 
 echo "Starting FastAPI application..."
